75ca213c12ede64d78dff6bb39dce4ca3f11206d
Dr.Guo <guoxiaozhong@outlook.com>
2024-01-06 22:59:12 +0800

    Win 托盘图标OK

--- ClipSharp.Win/App.xaml.cs
+++ ClipSharp.Win/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using H.NotifyIcon;
+using H.NotifyIcon.EfficiencyMode;
 
 namespace ClipSharp.Win;
 
@@ -9,4 +11,23 @@ namespace ClipSharp.Win;
 /// </summary>
 public partial class App : Application
 {
+    private TaskbarIcon _taskbar;
+
+
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        
+    }
+
+    /// <inheritdoc />
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        _taskbar = (TaskbarIcon) FindResource("TaskbarIcon");
+        // EfficiencyModeUtilities.SetEfficiencyMode(true);
+        _taskbar.ForceCreate();
+        // WindowExtensions.Hide(this Window window, enableEfficiencyMode: true) // default value
+        // WindowExtensions.Show(this Window window, disableEfficiencyMode: true) // default value
+        // TaskbarIcon.ForceCreate(bool enablesEfficiencyMode = true); // default value
+    }
 }
