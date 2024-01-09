ddde002db060f3907f6d14df5e903232fcb7c2bf
Dr.Guo <guoxiaozhong@outlook.com>
2024-01-09 14:22:05 +0800

    提交

--- ClipSharp.Win/View/ClipSelectWindow.xaml.cs
+++ ClipSharp.Win/View/ClipSelectWindow.xaml.cs
@@ -4,9 +4,12 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using Windows.Win32;
 using Windows.Win32.Graphics.Gdi;
+using ClipSharp.Win.Display;
 using ClipSharp.Win.ViewModel;
 using Microsoft.Extensions.Logging;
+using Clipboard = System.Windows.Clipboard;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+using TextDataFormat = System.Windows.TextDataFormat;
 
 namespace ClipSharp.Win.View;
 
@@ -15,6 +18,7 @@ public partial class ClipSelectWindow : Window
     private readonly ILogger<ClipSelectWindow> logger;
     private readonly ClipSelectViewModel model;
     private bool isEsc;
+    private bool isEnter;
 
     public ClipSelectWindow(ClipSelectViewModel model, ILogger<ClipSelectWindow> logger)
     {
@@ -92,10 +96,50 @@ public partial class ClipSelectWindow : Window
 
     private void ClipSelectWindow_OnDeactivated(object? sender, EventArgs e)
     {
-        if (this.isEsc)
+        if (this.isEsc||this.isEnter)
         {
             return;
         }
+        this.Close();
+    }
+
+    private void DisplayItem_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter)
+            return;
+
+        if (sender is System.Windows.Controls.ListViewItem item)
+        {
+            if (item.DataContext is ClipDisplayData data)
+            {
+                switch (data.Format)
+                {
+
+                    case ClipDisplayFormat.Text:
+                        Clipboard.SetText(data.Text);
+                        break;
+                    case ClipDisplayFormat.RichText:
+                        Clipboard.SetText(data.Text,TextDataFormat.Rtf);
+                        
+                        break;
+                    case ClipDisplayFormat.Html:
+                        break;
+                    case ClipDisplayFormat.Image:
+                        break;
+                    case ClipDisplayFormat.FileDropList:
+                        break;
+                    case ClipDisplayFormat.Audio:
+                        break;
+                    case ClipDisplayFormat.CustomFormat:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        
+        this.isEnter = true;
+        this.logger.LogInformation($"Log Id:");
         this.Close();
     }
 }
